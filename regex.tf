7cdd5a19ba008eaa1443e2292db7b44f4357df9f
6210743032 <6210743032@student.tu.ac.th>
2022-12-04 19:17:46 +0700

    IP regex

--- regex.tf
+++ regex.tf
@@ -1,6 +1,6 @@
 rule "aws_s3_bucket_name" {
   enabled = true
-  regex = "(?!(^xn--|.+-s3alias$))(?!.*[.]{2})^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$"
+  regex = "(?!(^xn--|.+-s3alias$))(?!.*[.]{2})(?!^(\d+\.)+\d+$)^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$"
   prefix = "my-org"
 }
 
